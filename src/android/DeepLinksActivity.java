686910d9111273b8150327f6b51c81a70d5f11cf
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-01-24 16:01:52 +0000

    Update DeepLinksActivity.java

--- src/android/DeepLinksActivity.java
+++ src/android/DeepLinksActivity.java
@@ -2,13 +2,24 @@ package com.example;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
+import android.util.Log;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class DeepLinksActivity extends CordovaPlugin {
+    private static final String TAG = "DeepLinksActivity";
+    private String assetLinksPath;
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        this.assetLinksPath = preferences.getString("ASSET_LINKS_PATH", "/.well-known/assetlinks.json");
+        Log.d(TAG, "Initialized with assetLinksPath: " + this.assetLinksPath);
+    }
 
     @Override
     public void onNewIntent(Intent intent) {
@@ -27,10 +38,12 @@ public class DeepLinksActivity extends CordovaPlugin {
     }
 
     private void handleDeepLink(Intent intent) {
+        Log.d(TAG, "handleDeepLink called");
         if (intent != null && Intent.ACTION_VIEW.equals(intent.getAction())) {
             Uri uri = intent.getData();
             if (uri != null) {
                 String uriString = uri.toString();
+                Log.d(TAG, "Deep link received: " + uriString);
                 webView.loadUrl("javascript:handleDeepLink('" + uriString + "');");
             }
         }
