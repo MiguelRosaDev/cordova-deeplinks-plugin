63b1bbb64b07ca2598f1a786dd23aebdd99b27bc
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-01-27 10:04:30 +0000

    Update DeepLinksActivity.java

--- src/android/DeepLinksActivity.java
+++ src/android/DeepLinksActivity.java
@@ -12,41 +12,48 @@ import org.json.JSONException;
 
 public class DeepLinksActivity extends CordovaPlugin {
     private static final String TAG = "DeepLinksActivity";
-    private String assetLinksPath;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        this.assetLinksPath = preferences.getString("ASSET_LINKS_PATH", "/.well-known/assetlinks.json");
-        Log.d(TAG, "Initialized with assetLinksPath: " + this.assetLinksPath);
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        handleDeepLink(intent);
+        Log.d(TAG, "DeepLinksActivity initialized");
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("handleDeepLink")) {
-            handleDeepLink(cordova.getActivity().getIntent());
-            callbackContext.success();
+            handleDeepLink(cordova.getActivity().getIntent(), callbackContext);
             return true;
         }
         return false;
     }
 
-    private void handleDeepLink(Intent intent) {
-        Log.d(TAG, "handleDeepLink called");
-        if (intent != null && Intent.ACTION_VIEW.equals(intent.getAction())) {
+    private void handleDeepLink(Intent intent, CallbackContext callbackContext) {
+        String intentString = intent.toString();
+        Log.d(TAG, "Handling deep link. Intent: " + intentString);
+        
+        if (intent.getAction() != null && intent.getAction().equals(Intent.ACTION_VIEW)) {
             Uri uri = intent.getData();
             if (uri != null) {
                 String uriString = uri.toString();
-                Log.d(TAG, "Deep link received: " + uriString);
+                Log.d(TAG, "Deep link URI: " + uriString);
+                callbackContext.success(uriString);
                 webView.loadUrl("javascript:handleDeepLink('" + uriString + "');");
+            } else {
+                Log.d(TAG, "Deep link URI is null");
+                callbackContext.error("Deep link URI is null");
             }
+        } else {
+            Log.d(TAG, "Not a VIEW intent");
+            callbackContext.error("Not a VIEW intent");
         }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d(TAG, "onNewIntent called");
+        handleDeepLink(intent, null);
     }
 }
 
